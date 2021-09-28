b2fafdc6ebaf4d375670fa63bd9ec7f9cee60d66
rahul31agrawal <rahul31agrawal@gmail.com>
2021-09-28 21:30:41 +0530

    quiz-updated

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ const quizForm = document.querySelector('.quiz-form');
 const submitAnswerBtn = document.querySelector('#submit-answer-btn');
 const outputEl = document.querySelector('#output');
 
-const correctAnswers = ["90°","right angled"];
+const correctAnswers = ["90°","right angled","Equilateral","Isosceles"];
 
 function calculateScore() {
 
