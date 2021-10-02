58d734661a556f9938c7fb7defebe4564fd2135f
rahul31agrawal <rahul31agrawal@gmail.com>
2021-10-02 15:20:56 +0530

    fixed-minor-bugs

--- istriangle.js
+++ istriangle.js
@@ -1,26 +1,39 @@
-const inputs = document.querySelectorAll('.angle-input');
-const isTriangleBtn = document.querySelector('#is-triangle-btn');
-const outputEl = document.querySelector('#output');
+const angleOne = document.querySelector("#angle-one");
+const angleTwo = document.querySelector("#angle-two");
+const angleThree = document.querySelector("#angle-three");
+const isTriangleBtn = document.querySelector("#is-triangle-btn");
+const outputDisplay = document.querySelector("#output");
 
-function calculateSumOfAngles(angle1,angle2,angle3){
-    const sumOfAngles = angle1 + angle2 + angle3;
-    return sumOfAngles;
+
+if (isTriangleBtn) {
+  isTriangleBtn.addEventListener("click", function () {
+    checkInput(angleOne, angleTwo, angleThree);
+  });
 }
 
-
-
-
-function isTriangle() {
-    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
-    if(sumOfAngles === 180){
-        
-        outputEl.innerText  = "Yay, The angles form a triangle";
-    }else{
-        
-        outputEl.innerText  = "oh oh! The angles don't form a triangle";
+function checkInput(inputOne, inputTwo, inputThree) {
+    if (inputOne.value && inputTwo.value && inputThree.value) {
+      if (
+        Number(inputOne.value) > 0 &&
+        Number(inputTwo.value) > 0 &&
+        Number(inputThree.value) > 0
+      ) {
+        //calculate
+        calculateSumOfAngles(inputOne, inputTwo, inputThree);
+      } else {
+        outputDisplay.innerText = "Input Value cannot be less than 1";
+      }
+    } else {
+      outputDisplay.innerText = "input fields cannot be empty";
     }
-}
-
-
-
-isTriangleBtn.addEventListener("click", isTriangle)
+  }
+  
+  function calculateSumOfAngles(valueOne, valueTwo, valueThree) {
+    let sumofAngles =
+      Number(valueOne.value) + Number(valueTwo.value) + Number(valueThree.value);
+    if (sumofAngles === 180) {
+      outputDisplay.innerText = "Is a Triangle";
+    } else {
+      outputDisplay.innerText = "Not a Triangle";
+    }
+  }
